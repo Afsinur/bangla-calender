c61822b41d380aa61631eb973d2ba193ca7d639c
afsinur <afsin.sayem1@gmail.com>
2024-03-14 10:17:00 +0600

    functional bangla to english calender

---
+++ script.js
@@ -0,0 +1,183 @@
+let countAgain = 0;
+let totalDatesAdded = 0;
+let dayIndex = 0;
+const months = [
+  { title: "বৈশাখ", totalDays: 31, altEng: "April", altEngTotalDays: 30 },
+  { title: "জ্যৈষ্ঠ", totalDays: 31, altEng: "May", altEngTotalDays: 31 },
+  { title: "আষাঢ়", totalDays: 31, altEng: "June", altEngTotalDays: 30 },
+  { title: "শ্রাবণ", totalDays: 31, altEng: "July", altEngTotalDays: 31 },
+  {
+    title: "ভাদ্র",
+    totalDays: 31,
+    altEng: "August",
+    altEngTotalDays: 31,
+  },
+  {
+    title: "আশ্বিন",
+    totalDays: 31,
+    altEng: "September",
+    altEngTotalDays: 30,
+  },
+  {
+    title: "কার্তিক",
+    totalDays: 30,
+    altEng: "October",
+    altEngTotalDays: 31,
+  },
+  {
+    title: "অগ্রহায়ণ",
+    totalDays: 30,
+    altEng: "November",
+    altEngTotalDays: 30,
+  },
+  {
+    title: "পৌষ",
+    totalDays: 30,
+    altEng: "December",
+    altEngTotalDays: 31,
+  },
+  { title: "মাঘ", totalDays: 30, altEng: "January", altEngTotalDays: 31 },
+  {
+    title: "ফাল্গুন",
+    totalDays: isLeapYear(new Date().getFullYear()) ? 30 : 29,
+    altEng: "February",
+    altEngTotalDays: isLeapYear(new Date().getFullYear()) ? 29 : 28,
+  },
+  { title: "চৈত্র", totalDays: 30, altEng: "March", altEngTotalDays: 31 },
+];
+const weekDays = [
+  { title: "রবি" },
+  { title: "সোম" },
+  { title: "মঙ্গল" },
+  { title: "বুধ" },
+  { title: "বৃহঃ" },
+  { title: "শুক্র" },
+  { title: "শনি" },
+];
+const select = {
+  one(sl) {
+    return document.querySelector(sl);
+  },
+};
+const components = {
+  title(month, i) {
+    let orMonth = months[months.length == i + 1 ? 0 : i + 1];
+
+    return `
+          <div class="p-2 text-center text-xl font-semibold text-gray-600">
+              <p>${month.title}-${new Date().getFullYear() - 593} - ${
+      month.altEng
+    }/${orMonth.altEng}</p>
+          </div>
+          `;
+  },
+  weeks(days) {
+    let htmls = ``;
+
+    for (let i = 0; i < days.length; i++) {
+      const day = days[i];
+      htmls += `
+          <div class="p-2 border">
+              <p class="text-center font-semibold text-gray-400">${day.title}</p>
+          </div>
+          `;
+    }
+
+    return htmls;
+  },
+  dates(month, _i) {
+    let orMonth = months[months.length == _i + 1 ? 0 : _i + 1];
+    let htmls = ``;
+
+    function reCount() {
+      countAgain++;
+      return countAgain;
+    }
+
+    if (_i > 0) {
+      totalDatesAdded = 0;
+
+      for (let i = 0; i < dayIndex; i++) {
+        totalDatesAdded++;
+
+        let insideHtmls = ``;
+        insideHtmls += ``;
+
+        htmls += `
+          <div class="border flex flex-col p-2 invisible">
+              <p class="text-xl font-semibold text-gray-600"></p>
+
+              <p class="self-end text-sm font-semibold text-gray-400 flex items-center justify-between w-full">${insideHtmls}</p>
+          </div>
+          `;
+      }
+    }
+
+    for (let i = 0; i < month.totalDays; i++) {
+      totalDatesAdded++;
+      dayIndex = totalDatesAdded % 7;
+
+      let insideHtmls = ``;
+      insideHtmls += `
+                <span class="">${
+                  i + 14 > month.altEngTotalDays
+                    ? orMonth.altEng.slice(0, 3)
+                    : month.altEng.slice(0, 3)
+                }</span>
+                <span>${
+                  i + 14 > month.altEngTotalDays
+                    ? reCount()
+                    : _i > 0
+                    ? reCount()
+                    : i + 14
+                }</span>
+              `;
+
+      htmls += `
+          <div class="border flex flex-col p-2">
+              <p class="text-xl font-semibold text-gray-600">${i + 1}</p>
+
+              <p class="self-end text-sm font-semibold text-gray-400 flex items-center justify-between w-full">${insideHtmls}</p>
+          </div>
+          `;
+
+      if (countAgain + 1 > month.altEngTotalDays) {
+        countAgain = 0;
+      }
+    }
+
+    return htmls;
+  },
+};
+function isLeapYear(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
+}
+months.forEach((month, i) => {
+  select.one("[data-show-months]").innerHTML += `
+          <div class="border px-4 py-2">
+              <div>
+                  ${components.title(month, i)}
+              </div>
+
+              <div class="grid gap-2 border grid-cols-7 p-2">
+                  ${components.weeks(weekDays)}
+              </div>
+
+              <div class="grid gap-2 border grid-cols-7 p-2">
+                  ${components.dates(month, i)}
+              </div>
+          </div>
+      `;
+});
